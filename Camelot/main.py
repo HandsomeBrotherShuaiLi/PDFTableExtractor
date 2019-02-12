f2130033ae9340d808bc03bbb0fedf73c5934a0a
TomHacker <3472661791.qq.com>
2019-02-12 21:34:41 +0800

    fixed bugs

--- Camelot/main.py
+++ Camelot/main.py
@@ -1,8 +1,21 @@
-import camelot
+import camelot,prettytable as pt
 def Camelot(path):
     tables=camelot.read_pdf(path,pages='all',flavor='stream')
+    tb=pt.PrettyTable()
+    tb.field_names=['1','2','3']
+    # print(tb)
     for i in range(1,28):
-        print('page {}'.format(i))
-        print(tables[i].data)
+        # print('page {}'.format(i))
+        # print(tables[i].data)
+        for j in tables[i].data:
+            if len(j)==3:
+                tb.add_row(j)
+            else:
+                res=[' ']
+                res.append(j[0])
+                res.append(j[1])
+                tb.add_row(res)
+    print(tb)
+
 if __name__=='__main__':
     Camelot('../data/list.pdf')
